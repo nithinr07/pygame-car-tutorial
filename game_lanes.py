6dcf3f3c183f7f3b8db5273dbbdbd329514e534b
Nithin Raj <nithin.raj@iiitb.org>
2021-03-14 15:56:46 +0530

    scrolling background code update

--- game_lanes.py
+++ game_lanes.py
@@ -10,32 +10,10 @@ import tkinter as tk
 from tkinter import ttk
 from numpy import random
 
-class Background():
-      def __init__(self):
-            self.bgimage = pygame.image.load('stars.png')
-            self.rectBGimg = self.bgimage.get_rect()
- 
-            self.bgY1 = 0
-            self.bgX1 = 0
- 
-            self.bgY2 = 0
-            self.bgX2 = self.rectBGimg.width
-          
-      def update(self, moving_speed):
-        self.bgX1 -= moving_speed
-        self.bgX2 -= moving_speed
-        if self.bgX1 <= -self.rectBGimg.width:
-            self.bgX1 = self.rectBGimg.width
-        if self.bgX2 <= -self.rectBGimg.width:
-            self.bgX2 = self.rectBGimg.width
-             
-      def render(self, DISPLAYSURF):
-         DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
-         DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
 class Car:
     def __init__(self, x, y, angle=0.0, length=4, max_steering=30, max_acceleration=1000.0):
         self.position = Vector2(x, y)
-        self.velocity = Vector2(0.0, 0.0)
+        self.velocity = Vector2(2.0, 0.0)
         self.angle = angle
         self.length = length
         self.max_acceleration = max_acceleration
@@ -89,21 +67,17 @@ class Game:
     def run(self, neuropy):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(current_dir, "spaceship.png")
-        # background_path = os.path.join(current_dir, "stars.png")
+        background_path = os.path.join(current_dir, "stars.png")
         car_image = pygame.image.load(image_path)
-        bg = Background()
         # scale = Vector2(car_image.getwidth()/self.width, car_image.get_height()/self.height)
         car_image = pygame.transform.scale(car_image, (100,75))
-        # background = pygame.image.load(background_path).convert()
+        background = pygame.image.load(background_path).convert()
         ppu = 16
         # Edges are (0,0); (self.width/ppu, 0); (0, self.height/ppu); (self.width/ppu, self.height/ppu)
         # car = Car(0, self.height/(2*ppu))
         car = Car(0, 0)
-        prev_time = 0;
-        txt = "End to End Time: "
-        txt_attention = "Attention:   "
-        txt_acceleration = "Acceleration: "
-        start = timer()
+        txt_attention = "Attention: "
+        txt_velocity = "Velocity: "
         while not self.exit:
             self.popupmsg("Wear headset properly")
             attention = (neuropy.attention)
@@ -119,47 +93,29 @@ class Game:
             # User input
             # pressed = pygame.key.get_pressed()
             # car.acceleration = ((attention - 50) / 10 )
-            car.velocity.x = (attention - 50) / 10  
+            car.velocity.y = (attention - 50) / 10  
             # + random.normal(loc=0, scale=5)
             # print(car.acceleration)
-            print(car.velocity.x)
+            print(car.velocity.y)
             # Logic
             car.update(dt)
-            bg.update(car.velocity.x)
             # print(start-timer())
             
-            # if(prev_time == 0):
-            #     counting_string = txt + "nil" 
-            # else:
-            #     counting_string = txt + str(prev_time)
-            # if(car.position.x >= self.width/ppu):
-            #     end = timer()
-            #     time_seconds = end - start
-            #     prev_time = time_seconds
-            #     counting_string = txt + str(time_seconds)
-            #     car.position.x = 0
-            #     # car.position.y = self.height/(2*ppu)
-            #     car.position.y = 0
-            #     car.velocity.x = 0
-            #     start = timer()
-            # if(car.position.x <= 0):
-            #     car.position.x = 0
-            #     if(car.velocity.x <= 0):
-            #         car.velocity.x = 0
+            if(car.position.y >= self.height):
+                car.position.y = self.height
+                # if(car.velocity.y <= 0):
+                #     car.velocity.y = 0
 
             # Drawing
-            # counting_text = self.font.render(str(counting_string), 1, (255,255,255))
             attention_text = self.font.render(str(txt_attention + str(attention)), 1, (255,255,255))
-            # acceleration_text = self.font.render(str(txt_acceleration + str(car.acceleration)), 1, (255,255,255))
+            velocity_text = self.font.render(str(txt_velocity + str(car.velocity.y)), 1, (255,255,255))
             self.screen.fill((0, 0, 0))
             rotated = pygame.transform.rotate(car_image, car.angle)
             rect = rotated.get_rect()
-            # self.screen.blit(background, -car.position * ppu - (rect.width / 2, rect.height / 2))
-            bg.render(self.screen)
-            self.screen.blit(rotated, (self.width/(0.3*ppu), self.height/(0.3*ppu)))
+            self.screen.blit(background, (-car.position.x * ppu - rect.width / 2, rect.height / 2))
+            self.screen.blit(rotated, (self.width/(0.3*ppu), car.position.y * ppu - self.height/(0.3*ppu)))
             self.screen.blit(attention_text, (self.width-190, 2))
-            # self.screen.blit(acceleration_text, (self.width-190, 20))
-            # self.screen.blit(counting_text, (2, 2))
+            self.screen.blit(velocity_text, (self.width-190, 20))
             
             pygame.display.flip()
             self.clock.tick(self.ticks)
